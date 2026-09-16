39cb4fdb052171d7ffe0a268e31f03e033018d7d
Muhammad hussain <hassan@Muhammads-MacBook-Air.local>
2026-09-16 12:47:42 +0500

    feat: configure S3 remote backend for terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "assignment07-tfstate-hassan"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
